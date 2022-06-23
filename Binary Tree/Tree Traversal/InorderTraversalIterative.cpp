9218c7b4e5c060e045200ce7ea3fb03b68a9636b
Antra Phukan <69572448+antrap@users.noreply.github.com>
2022-06-23 18:57:39 +0530

    Create InorderTraversalIterative.cpp

---
+++ Binary Tree/Tree Traversal/InorderTraversalIterative.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+     stack<TreeNode*> s;
+    TreeNode* curr=root;
+    vector<int> ans;
+    while(curr!=NULL && s.empty()==false){
+        while(curr!=NULL){
+            s.push(curr);
+            curr=curr->left;
+        }
+        curr=s.top();
+        s.pop();
+        ans.push_back(curr->val);
+        curr=curr->right;
+    }
+    return ans;
+    }
+};
+
+
+//T.C-O(N)
+//S.C-O(N)
